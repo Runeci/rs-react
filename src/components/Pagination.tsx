8a4e1308e73c1e52607bb3575669cfcfc296b1c4
daryia_kostenok <d.kastsianok@gmail.com>
2023-10-29 17:18:38 +0100

    feat: add pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface PaginationProps {
+  selectAmount: number;
+  currentPage: (value: string) => void;
+}
+
+export function Pagination({ selectAmount, currentPage }: PaginationProps) {
+  function setCurrentPage(event: React.ChangeEvent<HTMLSelectElement>) {
+    const target = event.target as HTMLSelectElement;
+    console.log(1,target.value);
+    currentPage(target.value)
+  }
+  return (
+    <div className="pagination">
+      Pagination
+      <select defaultValue="1" onChange={setCurrentPage}>
+        {Array.from(Array(selectAmount).keys()).map((i) => (
+          <option value={i} key={i}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
