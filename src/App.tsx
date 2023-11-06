9287cbd46fa4b2c13bec3829abe88627ceca7ff7
daryia_kostenok <d.kastsianok@gmail.com>
2023-11-06 18:25:55 +0100

    fix: substitute App class to function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import { Component } from 'react';
 import './App.css';
 import { ErrorBoundary } from './helpers/ErrorBoundary.tsx';
 import MainPage from './components/MainPage.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Details from './components/Details.tsx';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <ErrorBoundary>
-          <Routes>
-            <Route path="/" element={<MainPage />}>
-              <Route path="detail/:id" element={<Details />}></Route>
-            </Route>
-          </Routes>
-        </ErrorBoundary>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<MainPage />}>
+            <Route path="detail/:id" element={<Details />}></Route>
+          </Route>
+        </Routes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
 
 export default App;
