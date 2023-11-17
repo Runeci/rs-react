98f95d536863c8b7f5963940da04687c05ff970d
daryia_kostenok <d.kastsianok@gmail.com>
2023-11-17 17:59:53 +0100

    refactor: refactor unit tests Detail.tsx

--- src/test/Details.test.tsx
+++ src/test/Details.test.tsx
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect } from 'vitest';
+import { afterEach, beforeEach, describe, expect } from 'vitest';
 import { screen, waitFor } from '@testing-library/react';
 import Details from '../components/Details.tsx';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
@@ -11,12 +11,9 @@ import { server } from './server.tsx';
 import { API_URL } from '../services/apiSlice.tsx';
 
 describe('Details.tsx interactions', () => {
-  afterEach(() => {
-    vi.restoreAllMocks();
-  });
+  const user = userEvent.setup();
 
-  //Validate that clicking on a card opens a detailed card component;
-  it('should show details component when click on person card', async () => {
+  beforeEach(() => {
     renderWithProviders(
       <MemoryRouter>
         <Routes>
@@ -26,9 +23,13 @@ describe('Details.tsx interactions', () => {
         </Routes>
       </MemoryRouter>
     );
+  });
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
 
-    const user = userEvent.setup();
-
+  //Validate that clicking on a card opens a detailed card component;
+  it('should show details component when click on person card', async () => {
     await waitFor(() => {
       user.click(screen.getAllByTestId('person-container')[0]);
     });
@@ -36,14 +37,47 @@ describe('Details.tsx interactions', () => {
     expect(screen.getByTestId('detail-container')).toBeInTheDocument();
   });
 
-  //Make sure the detailed card component correctly displays the detailed card data
-  it('should correctly display card data', async () => {
+  //Ensure that clicking the close button hides the component
+  it('should hide details component on btn close click', async () => {
+    await waitFor(async () => {
+      await user.click(screen.getAllByTestId('person-container')[0]);
+      const closeButton = screen.getByTestId('detail-close-btn');
+      expect(closeButton).toBeInTheDocument();
+      await user.click(closeButton);
+    });
+
+    const detailCard = await screen.queryByTestId('detail-container');
+    expect(detailCard).not.toBeInTheDocument();
+  });
+
+  //Check that clicking triggers an additional API call to fetch detailed information.
+  it('should trigger an  API call to fetch detailed information', async () => {
+    const requestSpy = vi.fn();
+    server.events.on('request:start', ({ request }) => {
+      requestSpy(request.url);
+    });
+    await waitFor(async () => {
+      await user.click(screen.getAllByTestId('person-container')[0]);
+      expect(requestSpy).toHaveBeenCalledWith(`${API_URL}1`);
+    });
+  });
+});
+
+describe('Details.tsx', () => {
+  beforeEach(() => {
     renderWithProviders(
       <MemoryRouter initialEntries={['/detail/1']}>
         <Details />
       </MemoryRouter>
     );
+  });
 
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  //Make sure the detailed card component correctly displays the detailed card data
+  it('should correctly display card data', async () => {
     await waitFor(() => screen.getByTestId('detail-container'));
     expect(screen.getByTestId('detail-name')).toHaveTextContent(
       PERSON_MOCK.name
@@ -55,62 +89,6 @@ describe('Details.tsx interactions', () => {
 
   //Check that a loading indicator is displayed while fetching data;
   it('should show loading process while fetching', async () => {
-    renderWithProviders(
-      <MemoryRouter>
-        <Details />
-      </MemoryRouter>
-    );
-
     expect(screen.getByText('Details are loading...')).toBeInTheDocument();
-  });
-
-  //Check that clicking triggers an additional API call to fetch detailed information.
-  it('should trigger an  API call to fetch detailed information', async () => {
-    const user = userEvent.setup();
-    const requestSpy = vi.fn();
-    server.events.on('request:start', ({ request }) => {
-      requestSpy(request.url);
-    });
-    renderWithProviders(
-      <MemoryRouter>
-        <Routes>
-          <Route path={ROUTER_PATHS.root} element={<App />}>
-            <Route path={`${ROUTER_PATHS.detail}/:id`} element={<Details />} />
-          </Route>
-        </Routes>
-      </MemoryRouter>
-    );
-
-    await waitFor(async () => {
-      await user.click(screen.getAllByTestId('person-container')[0]);
-      expect(requestSpy).toHaveBeenCalledWith(`${API_URL}1`);
-    });
-  });
-});
-
-describe('Details.tsx', () => {
-  //Ensure that clicking the close button hides the component
-  it('should hide details component on btn close click', async () => {
-    renderWithProviders(
-      <MemoryRouter>
-        <Routes>
-          <Route path={ROUTER_PATHS.root} element={<App />}>
-            <Route path={`${ROUTER_PATHS.detail}/:id`} element={<Details />} />
-          </Route>
-        </Routes>
-      </MemoryRouter>
-    );
-
-    const user = userEvent.setup();
-
-    await waitFor(async () => {
-      await user.click(screen.getAllByTestId('person-container')[0]);
-      const closeButton = screen.getByTestId('detail-close-btn');
-      expect(closeButton).toBeInTheDocument();
-      await user.click(closeButton);
-    });
-
-    const detailCard = await screen.queryByTestId('detail-container');
-    expect(detailCard).not.toBeInTheDocument();
   });
 });
